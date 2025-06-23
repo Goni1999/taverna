e581a9636baadf1a16c29b116cb627661d90682d
ermaljanuzi <150066876+ermaljanuzi@users.noreply.github.com>
2025-06-23 12:05:40 -0700

    white to dark

--- script.js
+++ script.js
@@ -18,3 +18,47 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+  document.addEventListener('DOMContentLoaded', function () {
+    if (window.innerWidth < 768) {
+      const cards = document.querySelectorAll('.special-card');
+
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+          }
+        });
+      }, {
+        threshold: 0.6
+      });
+
+      cards.forEach(card => {
+        observer.observe(card);
+      });
+    }
+  });
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    if (window.innerWidth < 768) {
+      const cards = document.querySelectorAll('.special-card');
+
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+          }
+        });
+      }, {
+        threshold: 0.7 // më shumë se gjysma duhet të shfaqet
+      });
+
+      cards.forEach(card => {
+        observer.observe(card);
+      });
+    }
+  });
+
+
+
+
